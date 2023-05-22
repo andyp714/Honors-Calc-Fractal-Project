b7351a08d425e688904c2d7cbcad8b895afb7d0c
andyp714 <andypauley05@gmail.com>
2023-05-22 10:40:43 -0400

    setting up graphing but not yet functiona;

--- complexNumber.py
+++ complexNumber.py
@@ -5,6 +5,7 @@ class complexNumber():
         self.realValue = coefficientArray[0]
         self.imaginaryValue = coefficientArray[1]
         self.valueList = coefficientArray
+        self.distance = round(((self.realValue ** 2) + (self.imaginaryValue**2)) ** 0.5,6)
     
     def __add__(self, otherComplex):
         return complexNumber([self.realValue + otherComplex.realValue, self.imaginaryValue + otherComplex.imaginaryValue])
--- graphingMandlebrot.py
+++ graphingMandlebrot.py
@@ -3,11 +3,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    a = complexNumber([1,2])
-    b = complexNumber([-2,2])
-    c = a+b
-    print(c.valueList)
-    c.printComplex()
+    maxIterations = 100
+    pixelDensity = 1
+    realValues = np.linspace(-5,5, 10 * pixelDensity)
+    imaginaryValues =  np.linspace(-5,5, 10 * pixelDensity)
+
+
+    realPoints = []
+    imagPoints = []
+    for i in realValues:
+          for j in imaginaryValues:
+            converge = True
+            z = complexNumber([0,0])
+            c = complexNumber([i,j])
+            for k in range(maxIterations):
+                z = mandleFunc(z,c)
+                if z.distance > 10:
+                    converge = False
+                    print('broke')
+                    break
+            if converge == True:
+                print(i,j)
+                realPoints.append(i)
+                imagPoints.append(j)
+
+    plt.scatter(realPoints,imagPoints)
+    plt.show()
+              
+
+           
+    
+
+
+def convergeCheck(z,c):
+        return (z*z) + c
+
+
 
 if __name__ == "__main__":
         main()

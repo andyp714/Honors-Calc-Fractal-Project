0854bbfebba63fb5a73740b930e5ff1f5fcddebf
andyp714 <andypauley05@gmail.com>
2023-05-23 15:30:40 -0400

    minor changes

--- graphingMandlebrot.py
+++ graphingMandlebrot.py
@@ -9,8 +9,8 @@ def main():
     xmax = 1.5
     ymin = -1.5
     ymax = 1.5
-    realValues = np.linspace(xmin,xmax, int(xmax-xmin) * pixelDensity)
-    imaginaryValues =  np.linspace(ymin,ymax, int(ymax-ymin) * pixelDensity)
+    realValues = np.linspace(xmin,xmax, int(abs(xmax-xmin)) * pixelDensity)
+    imaginaryValues =  np.linspace(ymin,ymax, int(abs(ymax-ymin)) * pixelDensity)
 
     realPoints = []
     imagPoints = []
@@ -34,7 +34,7 @@ def main():
 def convergeCheck(z,c, maxIterations):
         for i in range(maxIterations):
             z = (z*z) + c
-            if z.distance > 10:
+            if z.distance > 2:
                 return False
                 break
         return True

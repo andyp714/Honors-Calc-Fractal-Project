a9584b15970e7c18cd04d7ae8e5a0ff8cb9a21c2
andyp714 <andypauley05@gmail.com>
2023-05-24 17:15:21 -0400

    julia set graph is working

--- graphingJulia.py
+++ graphingJulia.py
@@ -3,7 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-     pass
+     c = complexNumber([-0.162,1.04])
+     if convergeCheck(complexNumber([0,0]), c, maxIterations=40) == True:
+        maxIterations = 20
+        pixelDensity = 200
+        xmin = -5
+        xmax = 5
+        ymin = -1.5
+        ymax = 1.5
+        realValues = np.linspace(xmin,xmax, int(abs(xmax-xmin)) * pixelDensity)
+        imaginaryValues =  np.linspace(ymin,ymax, int(abs(ymax-ymin)) * pixelDensity)
+        realPoints = []
+        imagPoints = []
+        for i in realValues:
+          for j in imaginaryValues:
+            z = complexNumber([i,j])
+            if convergeCheck(z,c, maxIterations) == True:
+                realPoints.append(i)
+                imagPoints.append(j)
+
+        plt.scatter(realPoints,imagPoints, color='black', marker=',', s=1)
+        plt.tight_layout()
+        plt.show()
+     else:
+        print("Invalid Number")
+        
+
 
 
 def convergeCheck(z,c, maxIterations):
